41a547f1cd3f0360d29aa3722c177e532b73dd9f
Jan Biały <janbialy@hotmail.com>
2023-03-25 21:56:35 +0100

    added field canBePlaced, added scaling variable, added dropZone

--- src/Grid.js
+++ src/Grid.js
@@ -4,37 +4,39 @@ export default class Grid extends Phaser.GameObjects.GameObject{
 
     constructor(scene,x,y,texture, listeners){
         super(scene)
-        
+        let scale = 55
         this.letters = ['A','B','C','D','E','F','G','H','I','J'];   
         this.grid = scene.add.group();
         this.graphics = scene.add.graphics();
 
         for(var i=0;i<11;i++){
             for(var j=0;j<11;j++){
-                const sprite = this.grid.create(x+(i*50), y+(j*50), texture); 
+                const sprite = this.grid.create(x+(i*scale), y+(j*scale), texture); 
                 sprite.coordinates = {x: i, y: j};
                 sprite.ship=false; 
                 sprite.shooted=undefined;
+                sprite.canBePlaced = true;
                 if(Math.random()>0.5){
                     sprite.ship = true;
                 }
-                sprite.setScale(50/sprite.width);
+                sprite.setScale(scale/sprite.width);
                 sprite.setInteractive();
             }
         }
         this.grid.children.iterate((sprite)=>{
-            if(Math.floor((Math.round(sprite.x-x))/50) == 0 || Math.floor((Math.round(sprite.y-y))/50)==0 ){
-                if(Math.floor((Math.round(sprite.x-x))/50) == 0 && Math.floor((Math.round(sprite.y-y))/50)==0){
+            if(Math.floor((Math.round(sprite.x-x))/scale) == 0 || Math.floor((Math.round(sprite.y-y))/scale)==0 ){
+                if(Math.floor((Math.round(sprite.x-x))/scale) == 0 && Math.floor((Math.round(sprite.y-y))/scale)==0){
                     this.graphics.lineStyle(2, 0xffffff, 1);
                     this.graphics.lineBetween(sprite.x-22,sprite.y-22,sprite.x+22,sprite.y+22);
                 }
-                if(Math.floor((Math.round(sprite.x-x))/50) == 0 && Math.floor(((Math.round(sprite.y-y)))/50)!=0){
-                    scene.add.text(sprite.x-5,sprite.y-5,Math.floor((Math.round(sprite.y-y))/50),{fontSize:25})
+                if(Math.floor((Math.round(sprite.x-x))/scale) == 0 && Math.floor(((Math.round(sprite.y-y)))/scale)!=0){
+                    scene.add.text(sprite.x-5,sprite.y-5,Math.floor((Math.round(sprite.y-y))/scale),{fontSize:25})
                 }
-                if(Math.floor((Math.round(sprite.x-x))/50) != 0 && Math.floor((Math.round(sprite.y-y))/50) == 0 ){
-                    scene.add.text(sprite.x-5,sprite.y-5,this.letters[Math.floor((Math.round(sprite.x-x))/50)-1],{fontSize:25})
+                if(Math.floor((Math.round(sprite.x-x))/scale) != 0 && Math.floor((Math.round(sprite.y-y))/scale) == 0 ){
+                    scene.add.text(sprite.x-5,sprite.y-5,this.letters[Math.floor((Math.round(sprite.x-x))/scale)-1],{fontSize:25})
                 }
             }else{
+                sprite.input.dropZone = true;
                 if(listeners){
                     sprite.on('pointerdown',()=>{
                         if(sprite.shooted==undefined){
@@ -50,7 +52,7 @@ export default class Grid extends Phaser.GameObjects.GameObject{
                             }  
                         }
                         sprite.removeAllListeners()
-                        console.log(`Clicket field:  x: ${Math.floor((Math.round(sprite.x-x))/50)} y: ${Math.floor((Math.round(sprite.y-y))/50)}`);
+                        console.log(`Clicket field:  x: ${Math.floor((Math.round(sprite.x-x))/scale)} y: ${Math.floor((Math.round(sprite.y-y))/scale)}`);
                     });
                 }
             }
