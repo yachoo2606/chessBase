277fbbd702f20f7fc199654d0a4590d30fd4b5ef
Jan Biały <janbialy@hotmail.com>
2023-03-26 11:07:47 +0200

    game fits screen

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const config={
     type: Phaser.CANVAS,
     width: window.innerWidth,
     heihgt: window.innerHeight,
-    scaleMode:Phaser.Scale.ScaleModes.HEIGHT_CONTROLS_WIDTH,
+    scale:{
+        mode:Phaser.Scale.ENVELOP,
+        autoCenter:Phaser.Scale.CENTER_BOTH
+    },
     scene:[MenuScene,GameScene],
 };
 
