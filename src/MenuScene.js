27ec9f30ae14ef5e79a620bd244ae7e2f94443b2
Jan Biały <janbialy@hotmail.com>
2023-03-26 11:12:03 +0200

    button positions have been improved

--- src/MenuScene.js
+++ src/MenuScene.js
@@ -10,18 +10,18 @@ export default class MenuScene extends Phaser.Scene{
     create(){       
         //TITLE
         this.cameras.main.setBackgroundColor("#213769");
-        this.titleText = this.add.text(window.innerWidth/2, window.innerHeight/10, "WarShips",{ fontSize: '120px', fill: '#fff' })
+        this.titleText = this.add.text(window.innerWidth/2, window.innerHeight/5, "Battleship",{ fontSize: '120px', fill: '#fff' })
         this.titleText.setOrigin(0.5);
 
         // Add buttons
 
-        this.playButton = new TextButton(this,window.innerWidth/2,300,"Play",{ fontSize: '64px', fill: '#fff' },()=>{
+        this.playButton = new TextButton(this,window.innerWidth/2,window.innerHeight/2,"Play",{ fontSize: '64px', fill: '#fff' },()=>{
             // alert("Play");
             this.scene.start('gameScene');
         })
         this.add.existing(this.playButton)
 
-        this.exitButton = new TextButton(this,window.innerWidth/2,500,"Exit",{ fontSize: '64px', fill: '#fff' },()=>{
+        this.exitButton = new TextButton(this,window.innerWidth/2,window.innerHeight/2+200,"Exit",{ fontSize: '64px', fill: '#fff' },()=>{
             window.close();
         })
         this.add.existing(this.exitButton);
