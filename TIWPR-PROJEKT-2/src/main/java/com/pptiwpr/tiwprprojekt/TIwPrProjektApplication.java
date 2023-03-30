44b53c0736be9f544356ca182b4c0143e54f7ca3
inf144334 <jan.bialy@student.put.poznan.pl>
2023-03-30 17:48:35 +0200

    added space for second project

---
+++ TIWPR-PROJEKT-2/src/main/java/com/pptiwpr/tiwprprojekt/TIwPrProjektApplication.java
@@ -0,0 +1,13 @@
+package com.pptiwpr.tiwprprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TIwPrProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TIwPrProjektApplication.class, args);
+    }
+
+}
---
+++ TIWPR-PROJEKT-2/src/test/java/com/pptiwpr/tiwprprojekt/TIwPrProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pptiwpr.tiwprprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TIwPrProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
