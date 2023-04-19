9f06b98eaad2212dcd796f24f3b7e2014c6cf56f
Jan Biały <janbialy@hotmail.com>
2023-04-19 22:14:17 +0200

    added Token experience after 24 hours

--- TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/config/JwtService.java
+++ TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/config/JwtService.java
@@ -45,7 +45,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .setIssuer(userDetails.getAuthorities().toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
