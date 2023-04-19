f827c7beeb009c67790684ff636ebb5cc63dc28e
Jan Biały <janbialy@hotmail.com>
2023-04-19 12:43:37 +0200

    added error handlers

--- TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/exceptions/ExceptionsHandlers.java
+++ TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/exceptions/ExceptionsHandlers.java
@@ -1,10 +1,13 @@
 package pl.tiwpr.chessbase.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 @ControllerAdvice
 public class ExceptionsHandlers {
@@ -14,6 +17,20 @@ public class ExceptionsHandlers {
     @ResponseBody
     public String handleMissingDataException(MissingDataException e){
         return e.getMessage();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    public String handleDataIntegrityViolationException(DataIntegrityViolationException e){
+        return "Failed to create user. User with provided email already exists.";
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleAuthenticationException(AuthenticationException e){
+        return "Bad email or password.";
     }
 
 }
