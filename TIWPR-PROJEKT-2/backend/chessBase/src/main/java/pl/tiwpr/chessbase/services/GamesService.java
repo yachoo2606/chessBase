12a60ea9f1a65c42b06a71f438a37d551bd716c6
yachoo2606 <janbialy@hotmail.com>
2023-05-28 11:10:57 +0200

    game update moved from controller to gameService, added delete game endpoint

--- TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/services/GamesService.java
+++ TIWPR-PROJEKT-2/backend/chessBase/src/main/java/pl/tiwpr/chessbase/services/GamesService.java
@@ -3,12 +3,16 @@ package pl.tiwpr.chessbase.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pl.tiwpr.chessbase.model.Game;
+import pl.tiwpr.chessbase.model.Player;
 import pl.tiwpr.chessbase.model.Result;
 import pl.tiwpr.chessbase.repositories.GamesRepository;
 
+import java.time.LocalDate;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -16,9 +20,11 @@ import java.util.Optional;
 public class GamesService {
 
     private final GamesRepository gamesRepository;
+    private final PlayersService playersService;
 
-    public GamesService(GamesRepository gamesRepository) {
+    public GamesService(GamesRepository gamesRepository, PlayersService playersService) {
         this.gamesRepository = gamesRepository;
+        this.playersService = playersService;
     }
 
     public Game createGame(Game game){
@@ -44,8 +50,69 @@ public class GamesService {
     }
 
 
-    public ResponseEntity<?> updateGame(Game gameToUpdate) {
-        gamesRepository.save(gameToUpdate);
-        return ResponseEntity.ok().body("Game updated");
+    public ResponseEntity<?> updateGame(Long id, Long requestVersion,Map<String, Object> updates) {
+        Optional<Game> gameToEdit = gamesRepository.findGameById(id);
+        if(gameToEdit.isPresent()){
+            if(!requestVersion.equals(gameToEdit.get().getVersion())){
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body("You are trying to update out of date object get new game and try again.");
+            }
+
+            Game gameToUpdate = gameToEdit.get();
+
+            for(Map.Entry<String, Object> entry: updates.entrySet()){
+                String field = entry.getKey();
+                Object value = entry.getValue();
+
+                switch (field){
+                    case "event" -> gameToUpdate.setEvent((String) value);
+                    case "date" -> gameToUpdate.setDate(LocalDate.parse(value.toString()));
+                    case "round" ->{
+                        if(value instanceof Integer){
+                            gameToUpdate.setRound((Integer) value);
+                        }
+                    }
+                    case "whitePlayer" -> {
+                            Player player = playersService.getOneById(Long.valueOf((String) value));
+                            gameToUpdate.setWhitePlayer(player);
+                    }
+                    case "whiteELO" ->{
+                        if(value instanceof Integer){
+                            gameToUpdate.setWhiteELO((Integer) value);
+                        }
+                    }
+                    case "blackPlayer" ->{
+                            Player player = playersService.getOneById(Long.valueOf((String) value));
+                            gameToUpdate.setBlackPlayer(player);
+                    }
+                    case "blackELO" ->{
+                        if(value instanceof Integer){
+                            gameToUpdate.setBlackELO((Integer) value);
+                        }
+                    }
+                    case "result" -> {
+                        if(value instanceof Result){
+                            gameToUpdate.setResult((Result) value);
+                        }
+                    }
+                    case "pgn" -> gameToUpdate.setPgn((String) value);
+                }
+
+            }
+            gamesRepository.save(gameToUpdate);
+            return ResponseEntity.ok().body("Game updated");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Requested game not found");
+        }
+    }
+
+    public ResponseEntity<?> deleteGame(Long id) {
+        Optional<Game> gameToDelete = gamesRepository.findGameById(id);
+        if(gameToDelete.isPresent()){
+            log.warn("Game "+id+" deleted;");
+            gamesRepository.delete(gameToDelete.get());
+            return ResponseEntity.ok().body("Game deleted");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game not found");
+        }
     }
 }
